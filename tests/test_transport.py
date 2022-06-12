fc8d2a4f99bdc055d7b173342f4a6f503bae0612
Allan Crooks <allan@sixtyten.org>
2022-06-12 12:01:35 +0100

    More asserts around invoke and auth methods,

--- tests/test_transport.py
+++ tests/test_transport.py
@@ -2,6 +2,7 @@ import pytest
 from milky.transport import Transport
 from rtmapi import RtmRequestFailedException
 
+
 class TestTransport:
 
     # These are all made up, but they are used consistently across the
@@ -9,36 +10,42 @@ class TestTransport:
     API_KEY = "c90238bdea098efa089dfa9bda082903"
     SECRET = "b239dabcd9109e8f"
     TOKEN = "23d0cfec20adf80e0dddcf395032851085005318"
+    AUTH_URL = f"https://api.rememberthemilk.com/services/auth/?api_key={API_KEY}"
 
     @pytest.mark.vcr
     def test_desktop_auth(self):
-       r = Transport(self.API_KEY, self.SECRET)
-       r.start_auth()
-       r.finish_auth()
- 
+        FROB = "85cfb0d6aece75f477f99c1afe4b8d006977244e"
+        SIG = "4c4d26f4445c2740b08b74f566560277"
+        r = Transport(self.API_KEY, self.SECRET)
+        url = r.start_auth("write")
+        r.finish_auth()
+        assert url == f"{self.AUTH_URL}&perms=write&frob={FROB}&api_sig={SIG}"
+
     @pytest.mark.vcr
     def test_invalid_api_key(self):
-       r = Transport(self.API_KEY, self.SECRET)
-       with pytest.raises(RtmRequestFailedException):
+        r = Transport(self.API_KEY, self.SECRET)
+        with pytest.raises(RtmRequestFailedException):
             r.start_auth()
 
     @pytest.mark.vcr
     def test_invalid_signature(self):
-       r = Transport(self.API_KEY, self.SECRET)
-       with pytest.raises(RtmRequestFailedException):
+        r = Transport(self.API_KEY, self.SECRET)
+        with pytest.raises(RtmRequestFailedException):
             r.start_auth()
 
     @pytest.mark.vcr
     def test_bad_frob(self):
-       r = Transport(self.API_KEY, self.SECRET)
-       r.start_auth()
-       with pytest.raises(RuntimeError, match='could not finish auth'):
+        r = Transport(self.API_KEY, self.SECRET)
+        r.start_auth()
+        with pytest.raises(RuntimeError, match="could not finish auth"):
             r.finish_auth()
 
     @pytest.mark.block_network
     def test_mobile_auth(self):
+        SIG = "53a81b6a80e7f6319dd3f30b623a1f2c"
         r = Transport(self.API_KEY, self.SECRET)
-        r.start_auth(webapp=True)
+        url = r.start_auth(perms="delete", webapp=True)
+        assert url == f"{self.AUTH_URL}&perms=delete&api_sig={SIG}"
 
     @pytest.mark.vcr
     def test_given_token(self):
@@ -49,24 +56,49 @@ class TestTransport:
     def test_bad_token(self):
         r = Transport(self.API_KEY, self.SECRET, self.TOKEN)
         assert not r.authed
-        
+
     @pytest.mark.vcr
     def test_invoke_with_token(self):
         r = Transport(self.API_KEY, self.SECRET, self.TOKEN)
-        r.invoke('rtm.test.login')
-        r.invoke('rtm.time.parse', text='jun 19')
+        resp = r.invoke("rtm.test.login")
+        r.invoke("rtm.time.parse", text="jun 19")
 
-    @pytest.mark.vcr("TestTransport.test_desktop_auth.yaml", "TestTransport.test_invoke_with_token.yaml")
+        # Top-level element.
+        assert resp.tag == 'rsp'
+
+        # User element inside that.
+        kids = list(resp)
+        assert len(kids) == 1
+        assert kids[0].tag == 'user'
+        assert kids[0].attrib['id'] == '7447825'
+
+        # Username element inside that.
+        uname = list(kids[0])[0]
+        assert uname.tag == 'username'
+        assert uname.text == 'milkymark'
+
+    @pytest.mark.vcr(
+        "TestTransport.test_desktop_auth.yaml",
+        "TestTransport.test_invoke_with_token.yaml",
+    )
     def test_invoke_get_token(self):
         # Just combine auth part and then invoke.
         r = Transport(self.API_KEY, self.SECRET)
-        r.start_auth()
-        r.invoke('rtm.test.login')
-        r.invoke('rtm.time.parse', text='jun 19')
+        r.start_auth("read")
+        r.invoke("rtm.test.login")
+        resp = r.invoke("rtm.time.parse", text="jun 19")
+
+        # Top level element.
+        assert resp.attrib['stat'] == 'ok'
+
+        # Time element.
+        time = list(resp)[0]
+        assert time.attrib['precision'] == 'date'
+        assert time.text == '2022-06-19T00:00:00Z'
 
     @pytest.mark.block_network
     def test_invoke_no_token(self):
-        #should just raise an error, no request"
+        # should just raise an error, no request"
         r = Transport(self.API_KEY, self.SECRET)
-        with pytest.raises(RuntimeError, match='token is required'):
-            r.invoke('rtm.test.login')
+        with pytest.raises(RuntimeError, match="token is required"):
+            r.invoke("rtm.test.login")
