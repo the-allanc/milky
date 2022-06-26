d6d41c9451c39e39e39d3e9bec49d6aae1482b62
Allan Crooks <allan@sixtyten.org>
2022-06-26 11:53:28 +0100

    Make use of nox-poetry for safety.

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,3 @@
-import tempfile
-
 import nox
 from nox_poetry import session
 
@@ -68,15 +66,6 @@ def lint(session):
 
 @session
 def safety(session):
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install("safety")
-        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+    reqs = session.poetry.export_requirements()
+    session.install("safety")
+    session.run("safety", "check", f"--file={reqs}", "--full-report")
