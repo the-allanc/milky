f1fcb743d61deb82a79e51d191280b75e4f9efb3
Allan Crooks <allan@sixtyten.org>
2024-10-18 01:39:52 +0100

    Provide Milky object at top-level of package.

--- src/milky/__init__.py
+++ src/milky/__init__.py
@@ -2,6 +2,7 @@
 
 __version__ = '0.2.0'
 
+from milky.root import Milky
 from milky.transport import Identity, ResponseError, Transport
 
-__all__ = ['Identity', 'ResponseError', 'Transport']
+__all__ = ['Identity', 'Milky', 'ResponseError', 'Transport']
